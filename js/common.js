452e8783a6ed56ffbea0662a809cf0975fae212a
Gia <gia.jihye.j@gmail.com>
2021-03-23 17:12:56 +0900

    second commit

--- js/common.js
+++
@@ -1,6 +0,0 @@
-        window.onload=function(){
-            var link=document.querySelector(".link");
-            link.onclick=function(e){
-                return false;
-            }
-        }
